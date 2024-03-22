82a1e637eb83d288e28cef5a780007d47c7ce507
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2024-03-22 02:33:54 +0100

    Add expenses.js to services for initial data

---
+++ expenses.frontend/src/services/expenses.js
@@ -0,0 +1,12 @@
+export const GetExpenses = async (dispatch) => {
+  try {
+    // api call
+    const expenses = [
+      { id: 1, description: 'Grocersies', amount: 23.16 },
+      { id: 2, description: 'Gas', amount: 18.52 },
+      { id: 3, description: 'Student Loans', amount: 600 }
+    ]
+  } catch {
+    console.log('Error!');
+  }
+}
