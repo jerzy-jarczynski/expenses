129ce54db18de9ca903cafb28670df0d5563785e
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2024-03-23 22:11:07 +0100

    Add key.id to expenses in map function. TODO: unique id for each brand new expense

--- expenses.frontend/src/components/ExpenseList.js
+++ expenses.frontend/src/components/ExpenseList.js
@@ -14,7 +14,7 @@ export default () => {
   }, []);
 
   return expenses.map((e) => (
-    <div style={{ marginBottom: '1rem' }}>
+    <div key={e.id} style={{ marginBottom: '1rem' }}>
       <ListRow expense={e} />
     </div>
   ));
